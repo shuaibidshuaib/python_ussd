da47ab7b3292033e87c1ee955a8fa50bb3ad286d
Shuaib Ibraheem. <87182339+shuaibidshuaib@users.noreply.github.com>
2025-08-21 01:34:03 +0100

    Update firebase_utils.py

--- firebase_utils.py
+++ firebase_utils.py
@@ -1,12 +1,29 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+import os
+from firebase_admin import credentials, firestore, initialize_app
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+load_dotenv()
+
+service_account = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+}
+
+cred = credentials.Certificate(service_account)
+initialize_app(cred)
 db = firestore.client()
 
 def get_drug_status(batch_id):
-    doc = db.collection("drug_batches").document(batch_id).get()
+    doc = db.collection("drugs").document(batch_id).get()
     if doc.exists:
         return doc.to_dict()
     return None
